b64984ff237e5db291ba4c4c64917acc4d51f692
WJW <will.j.waller@gmail.com>
2018-08-29 19:55:27 -0400

    update

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,7 +1,7 @@
-var db = require("../models");
+// var db = require("../models");
 
 // Requiring path to so we can use relative routes to our HTML files
-var path = require("path");
+// var path = require("path");
 
 // Requiring our custom middleware for checking if a user is logged in
 var isAuthenticated = require("../config/middleware/isAuthenticated");
@@ -25,60 +25,60 @@ module.exports = function(app) {
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/members", isAuthenticated, function (req, res) {
+  app.get("/members", isAuthenticated, function(req, res) {
     res.render("members");
   });
 };
 
-module.exports = function(app) {
-  // Load index page
-  app.get("/", function(req, res) {
-    db.User.findAll({}).then(function(GTdb) {
-      res.render("indexTest", {
-        msg: "Welcome!",
-        examples: GTdb
-      });
-    });
-  });
+// module.exports = function(app) {
+//   // Load index page
+//   app.get("/", function(req, res) {
+//     db.User.findAll({}).then(function(GTdb) {
+//       res.render("indexTest", {
+//         msg: "Welcome!",
+//         examples: GTdb
+//       });
+//     });
+//   });
 
-  app.get("/library", function(req, res) {
-    res.render("library");
-  });
+//   app.get("/library", function(req, res) {
+//     res.render("library");
+//   });
 
-  app.get("/splash", function(req, res) {
-    res.render("splash");
-  });
+//   app.get("/splash", function(req, res) {
+//     res.render("splash");
+//   });
 
-  // Load example page and pass in an example by id
-  app.get("/profile/:username", function(req, res) {
-    db.User.findOne({ where: { userName: req.params.username } }).then(function(
-      profile
-    ) {
-      res.render("profile", {
-        user: profile
-      });
-    });
-  });
+//   // Load example page and pass in an example by id
+//   app.get("/profile/:username", function(req, res) {
+//     db.User.findOne({ where: { userName: req.params.username } }).then(function(
+//       profile
+//     ) {
+//       res.render("profile", {
+//         user: profile
+//       });
+//     });
+//   });
 
-  app.get("/aboutUs", function(req, res) {
-    res.render("aboutUs");
-  });
+//   app.get("/aboutUs", function(req, res) {
+//     res.render("aboutUs");
+//   });
 
-  app.get("/index", function(req, res) {
-    res.render("index");
-  });
+//   app.get("/index", function(req, res) {
+//     res.render("index");
+//   });
 
-  // Load example page and pass in an example by id
-  app.get("/profile/:id", function(req, res) {
-    db.User.findOne({ where: { id: req.params.id } }).then(function(GTdb) {
-      res.render("example", {
-        example: GTdb
-      });
-    });
-  });
+//   // Load example page and pass in an example by id
+//   app.get("/profile/:id", function(req, res) {
+//     db.User.findOne({ where: { id: req.params.id } }).then(function(GTdb) {
+//       res.render("example", {
+//         example: GTdb
+//       });
+//     });
+//   });
 
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
-  });
-};
+//   // Render 404 page for any unmatched routes
+//   app.get("*", function(req, res) {
+//     res.render("404");
+//   });
+// };
