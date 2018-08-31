ed8cc166b13baecc091dedf93fbaa037a87b1020
Dali Gala <habaresbit@gmail.com>
2018-08-31 15:15:41 -0400

    Facebook oauth

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -14,6 +14,22 @@ module.exports = function(app) {
   });
 
   app.get(
+    "/auth/facebook",
+    passport.authenticate("facebook", {
+      scope: ["public_profile", "email"]
+    })
+  );
+
+  app.get(
+    "/auth/facebook/callback",
+    passport.authenticate("facebook", { failureRedirect: "/login" }),
+    function(req, res) {
+      // Successful authentication, redirect home.
+      res.redirect("/");
+    }
+  );
+
+  app.get(
     "/auth/google",
     passport.authenticate("google", {
       scope: ["profile"]
