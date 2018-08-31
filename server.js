016c3626f6ce7c4d8d6340e6dd7534024d7df9e0
WJW <will.j.waller@gmail.com>
2018-08-31 16:48:14 -0400

    Update fullpage slider

--- server.js
+++ server.js
@@ -29,21 +29,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Socket setup & pass server
-var io = socket(server);
-io.on("connection", function(socket) {
-  console.log("made socket", socket.id);
 
-  // Handle chat event
-  socket.on("chat", function(data) {
-    // console.log(data);
-    io.sockets.emit("chat", data);
-  });
-
-  // Handle typing event
-  socket.on("typing", function(data) {
-    socket.broadcast.emit("typing", data);
-  });
-});
 // Handlebars
 app.engine(
   "handlebars",
@@ -77,7 +63,4 @@ db.sequelize.sync(syncOptions).then(function() {
   });
 });
 
-module.exports = io;
-module.exports = app;
-module.exports = giantBombKey;
-module.exports = steamKey;
+
